f6017ffc84e7373a2673db6e91f36b69171488b0
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-02-22 04:12:03 +0530

    Update The Password of Techlandia.java

--- Day 2/The Password of Techlandia.java
+++ Day 2/The Password of Techlandia.java
@@ -7,25 +7,22 @@ public class Main {
             map.put(password.get(i), map.getOrDefault(password.get(i), 0)+1);
         }
         boolean isEven = true;
-        for(Integer key : map.keySet()) {
-            if(map.get(key) % 2 != 0) {
-                isEven = false;
-                break;
-            }
-        }
-
         boolean isExactlyTwo = false;
-        for(Integer key : map.keySet()) {
-            if(map.get(key) == 2) {
-                isExactlyTwo = true;
-                break;
-            }
-        }
-
         int mostFrequent = Integer.MAX_VALUE;
         int max_freq = 0;
+
         for(Integer key : map.keySet()) {
             int freq = map.get(key);
+
+            if(freq % 2 != 0) {
+                isEven = false;
+            }
+
+            if(map.get(key) == 2) {
+                isExactlyTwo = true;
+            }
+
+
             if(freq > max_freq || (freq == max_freq && key < mostFrequent)) {
                 max_freq = freq;
                 mostFrequent = key;
@@ -54,3 +51,66 @@ public class Main {
         System.out.println(mostFrequentElement[0]);
     }
 }
+
+
+
+
+
+
+
+// import java.util.*;
+
+// public class Main {
+//     public static void validatePassword(List<Integer> password, String[] result, int[] mostFrequentElement) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         for(int i = 0; i < password.size(); i++) {
+//             map.put(password.get(i), map.getOrDefault(password.get(i), 0)+1);
+//         }
+//         boolean isEven = true;
+//         for(Integer key : map.keySet()) {
+//             if(map.get(key) % 2 != 0) {
+//                 isEven = false;
+//                 break;
+//             }
+//         }
+
+//         boolean isExactlyTwo = false;
+//         for(Integer key : map.keySet()) {
+//             if(map.get(key) == 2) {
+//                 isExactlyTwo = true;
+//                 break;
+//             }
+//         }
+
+//         int mostFrequent = Integer.MAX_VALUE;
+//         int max_freq = 0;
+//         for(Integer key : map.keySet()) {
+//             int freq = map.get(key);
+//             if(freq > max_freq || (freq == max_freq && key < mostFrequent)) {
+//                 max_freq = freq;
+//                 mostFrequent = key;
+//             }
+//         }
+
+//         if(isEven && isExactlyTwo) {
+//             result[0] = "VALID";
+//         } else {
+//             result[0] = "INVALID";
+//         }
+//         mostFrequentElement[0] = mostFrequent;
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         int N = scanner.nextInt();
+//         List<Integer> password = new ArrayList<>();
+//         for (int i = 0; i < N; i++) {
+//             password.add(scanner.nextInt());
+//         }
+//         String[] validationResult = new String[1];
+//         int[] mostFrequentElement = new int[1];
+//         validatePassword(password, validationResult, mostFrequentElement);
+//         System.out.println(validationResult[0]);
+//         System.out.println(mostFrequentElement[0]);
+//     }
+// }
